69bedc967682d00bfb4345d8f0254a251dde2c18
Andrew Stangl <andrewstangl@gmail.com>
2013-02-06 16:36:34 +0000

    prompting for resume screen, or not

--- modules/screen/init.zsh
+++ modules/screen/init.zsh
@@ -14,51 +14,70 @@ fi
 # Auto Start
 #
 
-if [[ -z "$STY" ]] && zstyle -t ':prezto:module:screen' prompt-start; then
-  echo "Do you wish to start a screen session?"
-  sleep 1
-  select yn in "Yes" "No"; do
-    case $yn in 
-      Yes) exec screen -S ZSH ;;
-      No) break ;;
-      *) break ;;
-    esac
-  done
-fi
-	
-if [[ -z "$STY" ]] && zstyle -t ':prezto:module:screen' prompt-resume; then
-  echo "Do you wish to resume last screen session?"
-  sleep 1
+function prompt_for_screen_start {
+
+  echo "---  ***  ***  *** --- "
+  echo "start screen session?"
   select yn in "Yes" "No"; do
     case $yn in 
       Yes) 
-        session="$(
-          screen -list 2> /dev/null \
-            | sed '1d;$d' \
-            | awk '{print $1}' \
-            | head -1)"
-        exec screen -x "$session"
+        echo "starting screen"
+        exec screen -S ZSH 
       ;;
-      No) break ;;
+      No) 
+        echo "not starting screen"
+        break 
+      ;;
       *) break ;;
     esac
   done
+
+}
+
+function prompt_for_screen_resume {
+
+  echo "---  ***  ***  *** --- "
+  echo "Existing screen session found: resume last session?"
+  select yn in "Yes" "No"; do
+    case $yn in 
+      Yes) 
+        exec screen -x "$session"; return 0
+      ;;
+      No) return 1 && break ;;
+      *) return 1 && break ;;
+    esac
+  done
+
+}
+
+session="$(
+  screen -list 2> /dev/null \
+  | sed '1d;$d' \
+  | awk '{print $1}' \
+  | head -1)"
+
+if [[ -n "$session" ]]; then
+  if [[ -z "$STY" ]] && zstyle -t ':prezto:module:screen' prompt-resume; then
+    prompt_for_screen_resume || prompt_for_screen_start
+  elif [[ -z "$STY" ]] && zstyle -t ':prezto:module:screen' prompt-start; then
+    prompt_for_screen_start
+  fi
+else 
+  if [[ -z "$STY" ]] && zstyle -t ':prezto:module:screen' prompt-start; then
+    prompt_for_screen_start
+  fi
 fi
 
 if [[ -z "$STY" ]] && zstyle -t ':prezto:module:screen' auto-start; then
 
   if [[ -n "$session" ]] && zstyle -t ':prezto:module:screen' resume; then
-    session="$(
-      screen -list 2> /dev/null \
-        | sed '1d;$d' \
-        | awk '{print $1}' \
-        | head -1)"
     exec screen -x "$session"
   else
 #    exec screen -a -A -U -D -R -m "$SHELL" -l
     exec screen -S ZSH
   fi
 fi
+
 
 #
 # Aliases
