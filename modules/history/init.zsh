5ee4dd75eb56370f6ef137562b2d4aff3ecf94f4
Andrew Stangl <andrewstangl@gmail.com>
2013-11-30 20:40:50 +0000

    bump history size

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -11,8 +11,8 @@
 #
 
 HISTFILE="${ZDOTDIR:-$HOME}/.zhistory"       # The path to the history file.
-HISTSIZE=5000                   # The maximum number of events to save in the internal history.
-SAVEHIST=7500                   # The maximum number of events to save in the history file.
+HISTSIZE=50000                   # The maximum number of events to save in the internal history.
+SAVEHIST=75000                   # The maximum number of events to save in the history file.
 
 #
 # Options
@@ -36,7 +36,7 @@ setopt HIST_BEEP                 # Beep when accessing non-existent history.
 #
 
 # Lists the ten most used commands.
-alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
+alias hist-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
 
 ## Pretty format history
 alias hist-date="history -E 0"
