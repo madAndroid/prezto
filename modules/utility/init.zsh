2608c3735aa88f4a4a7acf1bd9bfed3c412584fb
Andrew Stangl <andrewstangl@gmail.com>
2014-06-07 12:27:08 +0100

    switch to using tmux instead of screen

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -194,6 +194,9 @@ else
   alias http-serve='python -m SimpleHTTPServer'
 fi
 
+# Apt:
+alias purge-kernels="sudo apt-get remove --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')"
+
 #
 # Functions
 #
