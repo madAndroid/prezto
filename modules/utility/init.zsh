03ad9c108693ec6744e55d8f18d510571791eb3a
Andrew Stangl <andrewstangl@gmail.com>
2014-06-07 19:49:31 +0100

    change shell to have git prompt info on left as well

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -164,7 +164,7 @@ alias squid-purge-all="for url in \$(sudo cat /var/log/squid3/access.log | tail 
 alias http-serve='python -m SimpleHTTPServer'
 
 # Apt:
-alias purge-kernels="sudo apt-get remove --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')"
+#alias purge-kernels="sudo apt-get remove --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')"
 
 #
 # Functions
