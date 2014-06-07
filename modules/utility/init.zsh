f7459a797ea384c601ac7172c60c09c2c16cc047
Andrew Stangl <andrewstangl@gmail.com>
2014-06-07 12:27:08 +0100

    switch to using tmux instead of screen

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -163,6 +163,9 @@ alias squid-purge-all="for url in \$(sudo cat /var/log/squid3/access.log | tail 
 # Serves a directory via HTTP.
 alias http-serve='python -m SimpleHTTPServer'
 
+# Apt:
+alias purge-kernels="sudo apt-get remove --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')"
+
 #
 # Functions
 #
