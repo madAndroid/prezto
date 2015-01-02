d2f8fa2c025b8a325eea47a63f776c18e5124649
Andrew Stangl <andrewstangl@gmail.com>
2015-01-02 20:12:43 +0200

    helper functions, and key bindings for home/end on OSX

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -293,6 +293,12 @@ function vag-nuke {
     vagrant destroy -f $@
 }
 
+function vag-global-nuke {
+    for box in $(vagrant global-status | egrep -e 'running|poweroff'| cut -d' ' -f1); do 
+        vagrant destroy -f $box
+    done
+}
+
 function vag-stat {
     vagrant status $@
 }
@@ -311,6 +317,10 @@ function del-current-feature {
 
 function be {
     bundle exec $@
+}
+
+function del-known-host {
+    gsed -i -e "$1 d" $HOME/.ssh/known_hosts
 }
 
 #### GIT
