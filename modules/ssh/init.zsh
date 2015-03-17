e262dc48115da352c7df6914c14c7e5f82cdb228
Andrew Stangl <andrewstangl@gmail.com>
2015-03-17 06:15:56 +0000

    change ordering to ensure ssh loads before tmux, and ensure pid file get's created

--- modules/ssh/init.zsh
+++ modules/ssh/init.zsh
@@ -27,6 +27,7 @@ if [[ ! -S "$SSH_AUTH_SOCK" ]]; then
   # Start ssh-agent if not started.
   if ! ps -U "$USER" -o pid,ucomm | grep -q -- "${SSH_AGENT_PID:--1} ssh-agent"; then
     eval "$(ssh-agent | sed '/^echo /d' | tee "$_ssh_agent_env")"
+    echo $SSH_AGENT_PID > $HOME/.ssh/.auth_pid
   fi
 fi
 
